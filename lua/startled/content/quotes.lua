0ae92a4de9c796a768145b7f48d7388e69c6ae6e
Jesse Leite <jesseleite@gmail.com>
2025-07-14 22:41:57 -0400

    Quotes.

--- lua/startled/content/quotes.lua
+++ lua/startled/content/quotes.lua
@@ -1,4 +1,6 @@
 return {
   '"Not all those who wander are lost." <StartledNoWrap>— Bilbo Baggins</StartledNoWrap>',
-  '"You step into the road, and if you don’t keep your feet, there is no knowing where you might be swept off to." <StartledNoWrap>— J.R.R. Tolkien</StartledNoWrap>',
+  '"Do or do not. There is no try." <StartledNoWrap>— Yoda</StartledNoWrap>',
+  '"The love of knowledge is a kind of madness." <StartledNoWrap>— C.S. Lewis</StartledNoWrap>',
+  '"Everything passes, only truth remains." <StartledNoWrap>— Fyodor Dostoevsky</StartledNoWrap>',
 }
